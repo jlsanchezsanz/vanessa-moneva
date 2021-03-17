6edbd1e01508c9d67c0a0bccb8305ae30f7d03aa
jlsanchezsanz <jlsanchezsanz@hotmail.com>
2021-03-17 21:29:29 +0100

    refactor: turn slider logic into class

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,144 +1,173 @@
 import { debounce } from '../utils/debounce';
 
-const sliderContainer = document.querySelector('#slider');
-const rightSlider = document.querySelector('#right-slider');
-const leftSlider = document.querySelector('#left-slider');
-const prevButton = document.querySelector('#prev-button');
-const nextButton = document.querySelector('#next-button');
-const slidesLength = rightSlider.querySelectorAll('.slider__slide').length;
-const sliderDotsContainer = document.querySelector('#slider-dots-container');
-const rightSliderLinks = document.querySelectorAll('.slider__title--right');
-let sliderDotsLinks;
+export class Sliders {
+    createDotLink = (i) => {
+        const dotLink = document.createElement('a');
+        dotLink.classList.add('slider__dot-link');
+        dotLink.innerText = `0${i + 1}`;
+        dotLink.addEventListener('click', () => this.goToSlide(i));
+        return dotLink;
+    };
 
-let activeSlideIndexLeft = 0;
-let activeSlideIndexRight = 1;
+    createSliderDots = () => {
+        for (let i = 0; i < this.slidesLength; i++) {
+            const dotItem = document.createElement('li');
+            dotItem.classList.add('slider__dot-item');
+            const dotLink = this.createDotLink(i);
+            dotItem.appendChild(dotLink);
 
-const createDotLink = (i) => {
-    const dotLink = document.createElement('a');
-    dotLink.classList.add('slider__dot-link');
-    dotLink.innerText = `0${i + 1}`;
-    dotLink.addEventListener('click', () => goToSlide(i));
-    return dotLink;
-};
+            this.sliderDotsContainer.appendChild(dotItem);
+        }
 
-const createSliderDots = () => {
-    for (let i = 0; i < slidesLength; i++) {
-        const dotItem = document.createElement('li');
-        dotItem.classList.add('slider__dot-item');
-        const dotLink = createDotLink(i);
-        dotItem.appendChild(dotLink);
+        this.sliderDotsLinks = document.querySelectorAll('.slider__dot-link');
+    };
 
-        sliderDotsContainer.appendChild(dotItem);
-    }
+    updateActiveDot = () => {
+        const activeDot = document.querySelector('.slider__dot-link--active');
+        if (activeDot) {
+            activeDot.classList.remove('slider__dot-link--active');
+        }
 
-    sliderDotsLinks = document.querySelectorAll('.slider__dot-link');
-};
+        this.sliderDotsLinks[this.activeSlideIndexLeft].classList.add(
+            'slider__dot-link--active',
+        );
+    };
 
-const updateActiveDot = () => {
-    const activeDot = document.querySelector('.slider__dot-link--active');
-    if (activeDot) {
-        activeDot.classList.remove('slider__dot-link--active');
-    }
+    updateSlides = () => {
+        const sliderHeight = this.sliderContainer.clientHeight;
+        const leftSliderWidth = this.leftSlider.clientWidth;
 
-    sliderDotsLinks[activeSlideIndexLeft].classList.add(
-        'slider__dot-link--active',
-    );
-};
+        this.setLeftSlidePosition(
+            `-${this.activeSlideIndexLeft * leftSliderWidth}px`,
+        );
+        this.setRightSlidePosition(
+            `-${this.activeSlideIndexRight * sliderHeight}px`,
+        );
+    };
 
-const updateSlides = () => {
-    const sliderHeight = sliderContainer.clientHeight;
-    const leftSliderWidth = leftSlider.clientWidth;
+    goToSlide = (i) => {
+        this.activeSlideIndexLeft = i;
+        this.activeSlideIndexRight = i + 1;
+        if (this.activeSlideIndexRight > this.slidesLength - 1) {
+            this.activeSlideIndexRight = 0;
+        }
 
-    setLeftSlidePosition(`-${activeSlideIndexLeft * leftSliderWidth}px`);
-    setRightSlidePosition(`-${activeSlideIndexRight * sliderHeight}px`);
-};
+        this.updateSlides();
+        this.updateActiveDot();
+    };
 
-const goToSlide = (i) => {
-    activeSlideIndexLeft = i;
-    activeSlideIndexRight = i + 1;
-    if (activeSlideIndexRight > slidesLength - 1) {
-        activeSlideIndexRight = 0;
-    }
+    nextLeftSlide = () => {
+        this.activeSlideIndexLeft++;
+        if (this.activeSlideIndexLeft > this.slidesLength - 1) {
+            this.activeSlideIndexLeft = 0;
+        }
+    };
 
-    updateSlides();
-    updateActiveDot();
-};
+    nextRighSlidet = () => {
+        this.activeSlideIndexRight++;
+        if (this.activeSlideIndexRight > this.slidesLength - 1) {
+            this.activeSlideIndexRight = 0;
+        }
+    };
 
-const nextLeftSlide = () => {
-    activeSlideIndexLeft++;
-    if (activeSlideIndexLeft > slidesLength - 1) {
-        activeSlideIndexLeft = 0;
-    }
-};
+    prevLeftSlide = () => {
+        this.activeSlideIndexLeft--;
+        if (this.activeSlideIndexLeft < 0) {
+            this.activeSlideIndexLeft = this.slidesLength - 1;
+        }
+    };
 
-const nextRighSlidet = () => {
-    activeSlideIndexRight++;
-    if (activeSlideIndexRight > slidesLength - 1) {
-        activeSlideIndexRight = 0;
-    }
-};
+    prevRightSlide = () => {
+        this.activeSlideIndexRight--;
+        if (this.activeSlideIndexRight < 0) {
+            this.activeSlideIndexRight = this.slidesLength - 1;
+        }
+    };
 
-const prevLeftSlide = () => {
-    activeSlideIndexLeft--;
-    if (activeSlideIndexLeft < 0) {
-        activeSlideIndexLeft = slidesLength - 1;
-    }
-};
+    setLeftSlidePosition = (position) => {
+        this.leftSlider.style.transform = `translateX(${position})`;
+    };
 
-const prevRightSlide = () => {
-    activeSlideIndexRight--;
-    if (activeSlideIndexRight < 0) {
-        activeSlideIndexRight = slidesLength - 1;
-    }
-};
+    setRightSlidePosition = (position) => {
+        this.rightSlider.style.transform = `translateY(${position})`;
+    };
 
-const setLeftSlidePosition = (position) => {
-    leftSlider.style.transform = `translateX(${position})`;
-};
+    changeSlides = (direction) => {
+        if (direction === 'next') {
+            this.nextLeftSlide();
+            this.nextRighSlidet();
+        } else if (direction === 'prev') {
+            this.prevLeftSlide();
+            this.prevRightSlide();
+        }
 
-const setRightSlidePosition = (position) => {
-    rightSlider.style.transform = `translateY(${position})`;
-};
+        this.updateSlides();
+        this.updateActiveDot();
+    };
 
-const changeSlides = (direction) => {
-    if (direction === 'next') {
-        nextLeftSlide();
-        nextRighSlidet();
-    } else if (direction === 'prev') {
-        prevLeftSlide();
-        prevRightSlide();
-    }
+    debouncedScroll = debounce((e) => {
+        e.preventDefault();
+        if (e.deltaY > 0 || e.deltaX > 0) {
+            this.changeSlides('next');
+        } else {
+            this.changeSlides('prev');
+        }
+    }, 100);
 
-    updateSlides();
-    updateActiveDot();
-};
+    handleKeyUp = debounce((e) => {
+        if (e.keyCode === 37 || e.keyCode === 38) {
+            this.changeSlides('prev');
+        } else if (e.keyCode === 39 || e.keyCode === 40) {
+            this.changeSlides('next');
+        }
+    }, 100);
 
-const debouncedScroll = debounce((e) => {
-    e.preventDefault();
-    if (e.deltaY > 0 || e.deltaX > 0) {
-        changeSlides('next');
-    } else {
-        changeSlides('prev');
-    }
-}, 100);
+    getElementsFromDOM = () => {
+        this.sliderContainer = document.querySelector('#slider');
+        this.rightSlider = document.querySelector('#right-slider');
+        this.leftSlider = document.querySelector('#left-slider');
+        this.slidesLength = this.rightSlider.querySelectorAll(
+            '.slider__slide',
+        ).length;
+        this.sliderDotsContainer = document.querySelector(
+            '#slider-dots-container',
+        );
+        this.rightSliderLinks = document.querySelectorAll(
+            '.slider__title--right',
+        );
+        this.prevButton = document.querySelector('#prev-button');
+        this.nextButton = document.querySelector('#next-button');
+    };
 
-const handleKeyUp = debounce((e) => {
-    if (e.keyCode === 37 || e.keyCode === 38) {
-        changeSlides('prev');
-    } else if (e.keyCode === 39 || e.keyCode === 40) {
-        changeSlides('next');
-    }
-}, 100);
+    listenToEvents = () => {
+        this.prevButton.addEventListener('click', () =>
+            this.changeSlides('prev'),
+        );
+        this.nextButton.addEventListener('click', () =>
+            this.changeSlides('next'),
+        );
+        this.rightSliderLinks.forEach((link) =>
+            link.addEventListener('click', () => this.changeSlides('next')),
+        );
+        window.addEventListener('keyup', this.handleKeyUp);
+        window.addEventListener('wheel', this.debouncedScroll);
+    };
 
-createSliderDots();
-updateActiveDot();
+    initRightSlide = () => {
+        this.setRightSlidePosition(`-${this.activeSlideIndexRight * 100}vh`);
+    };
 
-setRightSlidePosition(`-${activeSlideIndexRight * 100}vh`);
+    init = () => {
+        this.activeSlideIndexLeft = 0;
+        this.activeSlideIndexRight = 1;
 
-prevButton.addEventListener('click', () => changeSlides('prev'));
-nextButton.addEventListener('click', () => changeSlides('next'));
-rightSliderLinks.forEach((link) =>
-    link.addEventListener('click', () => changeSlides('next')),
-);
-window.addEventListener('keyup', handleKeyUp);
-window.addEventListener('wheel', debouncedScroll);
+        this.getElementsFromDOM();
+        this.createSliderDots();
+        this.updateActiveDot();
+        this.listenToEvents();
+        this.initRightSlide();
+    };
+}
+
+const sliders = new Sliders();
+sliders.init();
--- webpack.common.js
+++ webpack.common.js
@@ -1,24 +1,29 @@
 module.exports = {
-  entry: {
-    main: './src/js/main.js',
-    vendor: './src/js/vendor.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: ['html-loader']
-      },
-      {
-        test: /\.(svg|png|jpg|gif)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[hash].[ext]',
-            outputPath: 'imgs'
-          }
-        }
-      }
-    ]
-  }
+    entry: {
+        main: './src/js/main.js',
+        vendor: './src/js/vendor.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: ['html-loader'],
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[hash].[ext]',
+                        outputPath: 'imgs',
+                    },
+                },
+            },
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/,
+            },
+        ],
+    },
 };
